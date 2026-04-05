e9977221aaf0153b187021861ce09a19c102f23a
HEXADEMON246 <giwaabdulrahman449@gmail.com>
2026-04-05 20:08:16 +0100

    assignmen done

---
+++ JAVASCRIPTS_TASKS/javascript_loops_task_2026_03_28.js
@@ -0,0 +1,33 @@
+//1
+for (let i = 1; i <= 20; i++) {
+    console.log(i);
+}
+
+//2
+for (let i = 1; i <= 50; i++) {
+    if (i % 2 === 0) {
+        console.log(i)
+    }
+}
+
+//4
+let num = prompt("Enter a number")
+
+for (let i = 1; i <=12; i++) {
+    console.log(num + "X" + i + "=" + (num * i));
+}
+
+//5
+for (let i = 10; i >= 1; i--) {
+    console.log(i);
+}
+
+//6
+for (let i = 1; i <= 30; i++) {
+    if (i % 2 !== 0) {
+        console.log(i)
+    } 
+}
+
+//7
+let correctNumber = 5; 

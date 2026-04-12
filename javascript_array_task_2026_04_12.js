fa94ef0595b5f10f5fe506ebfd26169d10270f9f
HEXADEMON246 <giwaabdulrahman449@gmail.com>
2026-04-12 21:27:36 +0100

    assignment done

---
+++ javascript_array_task_2026_04_12.js
@@ -0,0 +1,23 @@
+//beginner level
+
+//1
+let fruits = ["orange", "apple", "tangerine", "grape", "mango", ];
+console.log("First fruit", fruits[0]);
+console.log("Lastfruit", fruits[fruits.length - 1] );
+
+//2
+let numbers = [4, 8, 1, 15, 17];
+console.log("Length of number array",
+    numbers.length);
+
+//3
+numbers.push(20);
+console.log(numbers);
+
+//4
+numbers.pop();
+console.log(numbers);
+
+//5
+numbers.unshift(2);
+console.log(numbers);

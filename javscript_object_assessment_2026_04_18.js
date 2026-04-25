4fe4474465f14b0abd49fda8c3e475b68910fd39
HEXADEMON246 <giwaabdulrahman449@gmail.com>
2026-04-25 20:31:43 +0100

    assingment done

---
+++ javscript_object_assessment_2026_04_18.js
@@ -0,0 +1,45 @@
+//1
+const student = {
+  name: "Abdul",
+  age: 20,
+  grade: "A"
+};
+
+// Access properties
+console.log(student.name);   // Abdul
+console.log(student.age);    // 20
+console.log(student.grade);  // A
+
+//2
+student.school = "UI";  // University of Ibadan (e.g)
+
+console.log(student);
+// { name: "Abdul", age: 20, grade: "A", school: "UI" }
+
+//3
+student.grade = "A+";
+
+console.log(student.grade); // A+
+
+//4
+delete student.age;
+
+console.log(student.age); // undefined
+
+// Check if property still exists
+console.log("age" in student); // false
+
+//5
+const user = {
+  name: "Abdul",
+  age: 20,
+  address: {
+    street: "Bodija Road",
+    city: "Ibadan",
+    country: "Nigeria"
+  }
+};
+
+// Access nested values
+console.log(user.address.city);     // Ibadan
+console.log(user.address.country);  // Nigeria

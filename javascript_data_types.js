a6c57bf0dacc1d035331ebdef4158c2e40a23b17
HEXADEMON246 <giwaabdulrahman449@gmail.com>
2026-07-12 20:53:13 +0100

    assignment done

--- javascript_data_types.js
+++ javascript_data_types.js
@@ -107,7 +107,7 @@ let myVariable; // declaring a variable without assigning a value
 console.log(myVariable); // Output: undefined
 
 /**
- * create a file called javascript_data_types.js and answer the following questions:
+ * create a file called and answer the following questions:
  * 1. Variable Types Identification
 
 List three variables you would create to store:
